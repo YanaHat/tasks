c29d94763089473fa44ce3316d20e2942253427c
Your Name <you@example.com>
2026-02-24 16:45:57 +0100

    feat: integrate map into weather widget

--- Weather/weather-widget/src/render.ts
+++ Weather/weather-widget/src/render.ts
@@ -11,6 +11,7 @@ export function renderWeather(data: WeatherResponse) {
     });
     const iconCode = data.weather[0].icon;
     const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    const mapUrl = `https://www.google.com/maps/search/?api=1&query=${data.name}`;
 
     container.innerHTML = `
         <div class="inf">
@@ -41,6 +42,15 @@ export function renderWeather(data: WeatherResponse) {
                 <p>Feels Like</p>
                 <h3>${Math.round(data.main.feels_like)}°C</h3>
             </div>
+        </div>
+        <div class="map card">
+            <img class="w-full h-full object-cover opacity-60 grayscale hover:grayscale-0 transition-all duration-700" data-alt="Topographic map view of New York City landscape" data-location="New York City" src="https://lh3.googleusercontent.com/aida-public/AB6AXuDQGAJLuVNdOKKx5FWBnQgRNlaPqrdo99p6HXV6uJS9hJyn91KxTIp4xdJQt7adWWjoikjhbr9PrRKLXFPIb7VUk8juy0BZrKt5qSOFQA3OIK3ejMcA9a_ZWTBWyWfj7Ok1FI_aa7YGxgufRCNd9hEqxm7Ou-7sh0JEjnX-DLgSy_XKjrL61j2IDZ06lnbFViIBp2eo1mwxlVYoMnth_Ln4k-enmZ3zO5kwfLbFkKZMevgjt7MG9NOxa2-fYtWIE1-CNwVwQccLFgE">
+            <div class="mapBtn">
+                <div>
+                    <span class="material-symbols-outlined text-primary">location_on</span>
+                    <a href="${mapUrl}" target="_blank" class="map-btn">View detailed map</a>
+                </div>    
+            </div>    
         </div>
     `;
 }
