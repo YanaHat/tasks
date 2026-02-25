b9309f4cc616bcc15c38b331baa18e01e30a6e17
Your Name <you@example.com>
2026-02-25 14:16:15 +0100

    feat(history): add history structure and functionalities

--- Weather/weather-widget/src/main.ts
+++ Weather/weather-widget/src/main.ts
@@ -1,5 +1,6 @@
 import { fetchWeather } from "./api";
 import { renderWeather } from "./render";
+import { saveCity, getHistory } from "./storage";
 
 const input = document.getElementById("cityInput") as HTMLInputElement;
 const button = document.getElementById("searchBtn") as HTMLButtonElement;
@@ -13,8 +14,37 @@ button.addEventListener("click", async () => {
     errorDiv.textContent = "Loading...";
     const data = await fetchWeather(city);
     renderWeather(data);
+    saveCity(city);  
+    renderHistory();
     errorDiv.textContent = "";
   } catch (error) {
     errorDiv.textContent = "City not found, please try again";
   }
 });
+
+function renderHistory() {
+  const historyContainer = document.getElementById("historyContainer")!;
+  const cities = getHistory();
+
+  historyContainer.innerHTML = `
+    <span class="history">Recent:</span>
+    ${cities
+      .map(
+        (city) =>
+          `<button class="history-item" data-city="${city}"><span class="material-symbols-outlined text-xs">history</span>${city}</button>`
+      )
+      .join("")}
+  `;
+
+  document.querySelectorAll(".history-item").forEach((btn) => {
+    btn.addEventListener("click", async () => {
+      const city = (btn as HTMLElement).dataset.city!;
+      input.value = city;
+
+      const data = await fetchWeather(city);
+      renderWeather(data);
+    });
+  });
+}
+
+renderHistory();
--- Weather/weather-widget/src/render.ts
+++ Weather/weather-widget/src/render.ts
@@ -43,7 +43,7 @@ export function renderWeather(data: WeatherResponse) {
                 <h3>${Math.round(data.main.feels_like)}°C</h3>
             </div>
         </div>
-        <div class="map card">
+        <div class="map">
             <img class="w-full h-full object-cover opacity-60 grayscale hover:grayscale-0 transition-all duration-700" data-alt="Topographic map view of New York City landscape" data-location="New York City" src="https://lh3.googleusercontent.com/aida-public/AB6AXuDQGAJLuVNdOKKx5FWBnQgRNlaPqrdo99p6HXV6uJS9hJyn91KxTIp4xdJQt7adWWjoikjhbr9PrRKLXFPIb7VUk8juy0BZrKt5qSOFQA3OIK3ejMcA9a_ZWTBWyWfj7Ok1FI_aa7YGxgufRCNd9hEqxm7Ou-7sh0JEjnX-DLgSy_XKjrL61j2IDZ06lnbFViIBp2eo1mwxlVYoMnth_Ln4k-enmZ3zO5kwfLbFkKZMevgjt7MG9NOxa2-fYtWIE1-CNwVwQccLFgE">
             <div class="mapBtn">
                 <div>
