49a1ef19d80f016feb1ae5adb925f6340bc60176
Your Name <you@example.com>
2026-02-27 13:52:10 +0100

    finalize project

--- Weather/weather-widget/src/render.ts
+++ Weather/weather-widget/src/render.ts
@@ -134,18 +134,48 @@ export function renderWeather(data: WeatherResponse) {
 }
 
 export function renderLoading() {
-  const container = document.getElementById("weatherContainer")!;
-  clearContainer(container);
-
-  document.querySelector(".container")?.classList.add("loading");
-  container.classList.add("weatherLoad");
-  document.querySelector(".search")?.classList.remove("searchError");
-
-  const loading = createElement("div", "inf");
-  const text = createElement("h2", "nameCountry", "Loading...");
-  loading.appendChild(text);
-
-  container.appendChild(loading);
+    document.querySelector(".container")?.classList.add("loading");
+    document.getElementById("weatherContainer")?.classList.add("weatherLoad");
+    document.querySelector(".search")?.classList.remove("searchError");
+    const container = document.getElementById("weatherContainer")!;
+    container.innerHTML = `
+        <div class="inf">
+            <h2 class="nameCountry">     </h2>
+            <p class="data">  </p>
+            <h1 class="temp">   <span>   </span></h1>
+            <p class="description">  </p>
+        </div>    
+        <div class="grid">
+            <div class="card">
+                <span class="material-symbols-outlined text-primary mb-2"></span>
+                <p></p>
+                <h3></h3>
+            </div>
+            <div class="card">
+                <span class="material-symbols-outlined text-primary mb-2"></span>
+                <p></p>
+                <h3></h3>
+            </div>
+            <div class="card">
+                <span class="material-symbols-outlined text-primary mb-2"></span>
+                <p></p>
+                <h3></h3>
+            </div>
+            <div class="card">
+                <span class="material-symbols-outlined text-primary mb-2"></span>
+                <p></p>
+                <h3></h3>
+            </div>
+        </div>
+        <div class="map">
+            <div class="mapBtn">
+                <div>
+                    <span class="material-symbols-outlined text-primary">location_on</span>
+                    <a href="" target="_blank" class="map-btn">Loading map data ...</a>
+                </div>    
+            </div>    
+        </div>
+    `;
 }
 
 export function renderError(message: string) {
@@ -170,6 +200,58 @@ export function renderError(message: string) {
 
   error.append(icon, title, subtitle);
   wrapper.appendChild(error);
+
+  container.appendChild(wrapper);
+
+  const grid = createElement("div", "grid");
+
+  grid.append(
+    createDetailCard(
+      "humidity_percentage",
+      "Humidity",
+      `-- %`
+    ),
+    createDetailCard(
+      "air", 
+      "Wind", 
+      `-- km/h`),
+    createDetailCard(
+      "compress",
+      "Pressure",
+      `-- hPa`
+    ),
+    createDetailCard(
+      "thermostat",
+      "Feels Like",
+      `-- °C`
+    )
+  );
+
+  const map = createElement("div", "map");
+
+  const img = createElement("img",
+    "w-full h-full object-cover opacity-60 grayscale hover:grayscale-0 transition-all duration-700"
+  ) as HTMLImageElement;
+
+  img.src =
+    "https://lh3.googleusercontent.com/aida-public/AB6AXuDQGAJLuVNdOKKx5FWBnQgRNlaPqrdo99p6HXV6uJS9hJyn91KxTIp4xdJQt7adWWjoikjhbr9PrRKLXFPIb7VUk8juy0BZrKt5qSOFQA3OIK3ejMcA9a_ZWTBWyWfj7Ok1FI_aa7YGxgufRCNd9hEqxm7Ou-7sh0JEjnX-DLgSy_XKjrL61j2IDZ06lnbFViIBp2eo1mwxlVYoMnth_Ln4k-enmZ3zO5kwfLbFkKZMevgjt7MG9NOxa2-fYtWIE1-CNwVwQccLFgE";
+
+  const mapBtn = createElement("div", "mapBtn");
+  const mapInner = createElement("div");
+
+  const locationIcon = createElement(
+    "span",
+    "material-symbols-outlined text-primary",
+    "map"
+  );
+
+  const link = createElement("span", "errorMapMessage", "Map unavailable") as HTMLAnchorElement;
+
+  mapInner.append(locationIcon, link);
+  mapBtn.appendChild(mapInner);
+  map.append(img, mapBtn);
+
+  wrapper.append(error, grid, map);
 
   container.appendChild(wrapper);
 }
